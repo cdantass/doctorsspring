b652f04f3fa19062c2454b1f4dd84b6984285be7
cauaunit <caua.dantas@souunit.com.br>
2026-04-14 11:59:12 -0300

    funcionando

--- src/main/java/com/med/doctorss/infra/security/TokenService.java
+++ src/main/java/com/med/doctorss/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.med.doctorss.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.med.doctorss.entity.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,20 @@ public class TokenService {
 
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar o token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT){
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("API Doctors")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
--- src/main/java/com/med/doctorss/infra/security/securityFilter.java
+++ src/main/java/com/med/doctorss/infra/security/securityFilter.java
@@ -1,9 +1,13 @@
 package com.med.doctorss.infra.security;
 
+import com.med.doctorss.entity.user.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,19 +16,32 @@ import java.io.IOException;
 @Component
 public class securityFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
 
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var user = repository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if (authorizationHeader == null){
-            throw new RuntimeException("Token não enviado no cabeçalho Authorization");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ","");
         }
-        return authorizationHeader;
+        return null;
     }
 }
