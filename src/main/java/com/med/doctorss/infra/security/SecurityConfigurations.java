2842ae05f19e8957983054e2001555dc24682fc0
cauaunit <caua.dantas@souunit.com.br>
2026-05-11 10:58:22 -0300

    erros

--- src/main/java/com/med/doctorss/infra/security/SecurityConfigurations.java
+++ src/main/java/com/med/doctorss/infra/security/SecurityConfigurations.java
@@ -30,6 +30,7 @@ public class SecurityConfigurations {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST,"/login").permitAll().anyRequest().authenticated()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                 )
                         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
